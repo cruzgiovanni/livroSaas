748baaee5e73e8b0e8fdb78061a5dc3f9ac3fad5
Giovanni <giovannicruzvinhedo@gmail.com>
2025-02-25 17:19:22 -0300

    feat: user auth with session's cookie

--- auth.ts
+++ auth.ts
@@ -1,5 +1,26 @@
 import NextAuth from "next-auth"
+import Credentials from "next-auth/providers/credentials"
+import { findUserByCredentials } from "./lib/user"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Credentials({
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        console.log(credentials)
+
+        // auth logic here
+        // find user with credentials
+        const user = findUserByCredentials(
+          credentials.email as string,
+          credentials.password as string
+        )
+
+        return user
+      },
+    }),
+  ],
 })
